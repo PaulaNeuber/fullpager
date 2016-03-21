0c782357db68ce51b44d141674f03fa77be970c6
CodingJo <jonathan.dudek@web.de>
2016-03-21 20:23:02 +0100

    next changes Jonathan

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,19 +1,16 @@
   <footer class="footer cf" role="contentinfo">
 
     <div class="copyright">
-      <?php echo $site->copyright()->kirbytext() ?>
+      <a href="http://localhost/fullpager-master/impressum"><?php echo $site->copyright()->kirbytext() ?></a>
     </div>
-<<<<<<< HEAD
     <div class="container">
+      <h4><p>Wir danken unseren Sponsoren und Förderern:</p></h4><br />
       <img src="assets\images\Webseite Inhalte\Logo_Gessler.jpg">
       <img src="assets\images\Webseite Inhalte\SL_Logo.png">
       <img src="assets\images\Webseite Inhalte\Stadtwerke.png">
       <img src="assets\images\Webseite Inhalte\BodenseekreisStiftung.jpg">
       <img src="assets\images\Webseite Inhalte\ZUG.png">
     </div>
-=======
-
->>>>>>> 5b6d2e1af00952f55bfdf2d996a99f3b5016e5b0
     <div class="colophon">
       <a href="http://getkirby.com/made-with-kirby-and-love">Made with Kirby and <b>♥</b></a>
     </div>
