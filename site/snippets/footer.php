27798868f5cdc18d920be7c667f4cfa9be5da00a
CodingJo <jonathan.dudek@web.de>
2016-03-22 13:03:30 +0100

    footer galery jonathan

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,13 +3,14 @@
     <div class="copyright">
       <a href="http://localhost/fullpager-master/impressum"><?php echo $site->copyright()->kirbytext() ?></a>
     </div>
+
     <div class="container">
       <h4><p>Wir danken unseren Sponsoren und Förderern:</p></h4><br />
-      <img src="assets\images\Webseite Inhalte\Logo_Gessler.jpg">
-      <img src="assets\images\Webseite Inhalte\SL_Logo.png">
-      <img src="assets\images\Webseite Inhalte\Stadtwerke.png">
-      <img src="assets\images\Webseite Inhalte\BodenseekreisStiftung.jpg">
-      <img src="assets\images\Webseite Inhalte\ZUG.png">
+      <img src="assets\images\Webseite Inhalte\ZUG.png" class="responsive-image">
+      <img src="assets\images\Webseite Inhalte\Stadtwerke.png" class="responsive-image">
+      <img src="assets\images\Webseite Inhalte\Logo_Gessler.jpg" class="responsive-image">
+      <img src="assets\images\Webseite Inhalte\SL_Logo.png" class="responsive-image">
+      <img src="assets\images\Webseite Inhalte\BodenseekreisStiftung.jpg" class="responsive-image">
     </div>
     <div class="colophon">
       <a href="http://getkirby.com/made-with-kirby-and-love">Made with Kirby and <b>♥</b></a>
---
+++ site/templates/galery.php
@@ -0,0 +1,19 @@
+<?php snippet('header') ?>
+
+  <main class="main" role="main">
+
+    <div class="text">
+      <h1><?php echo $page->title()->html() ?></h1>
+      <?php echo $page->text()->kirbytext() ?>
+    </div>
+  
+  <div class="container">
+  <img src="content/1-galery/2015_Casino.jpg" class="responsive-image" style="margin:30px 0px 30px 0px">
+  <img src="content/1-galery/2015_Casino.jpg" class="responsive-image" style="margin:30px 0px 30px 0px">
+  <img src="content/1-galery/2015_Casino.jpg" class="responsive-image" style="margin:30px 0px 30px 0px">
+  <img src="content/1-galery/2015_Casino.jpg" class="responsive-image" style="margin:30px 0px 30px 0px">
+  <img src="content/1-galery/2015_Casino.jpg" class="responsive-image" style="margin:30px 0px 30px 0px" width="50%">
+  </div>
+  </main>
+
+<?php snippet('footer') ?>
