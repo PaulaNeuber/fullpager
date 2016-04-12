74346c1e427d978cbd414502b72e4abc17ee42bc
Paula Neuber <p.neuber@zeppelin-university.net>
2016-04-12 15:40:42 +0200

    Update Startseite
    - Einbindung Facebook
    - Überschrift

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,12 +5,23 @@
     <div class="title">
       <?php echo $page->title()->kirbytext() ?>
     </div>
-    <div class="text">
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
     <hr>
-
+<table style="width:90%">
+  <tr>
+    <td><div class="text">
+      <?php echo $page->text()->kirbytext() ?>
+    </div></td>
+    <td><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flangenachtfn&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></td>
+  </tr>
+  <tr>
+    
+  </tr>
+ 
+</table>
 
   </main>
+
+
+
 
 <?php snippet('footer') ?>
