be38d8b569f1745e3367f686fa8c82238da55b91
CodingJo <jonathan.dudek@web.de>
2016-04-15 21:41:32 +0200

    latest changes

---
+++ accounts/paula.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: paula
+email: neuber.paula@googlemai.com
+password: >
+  $2a$10$xhZiO3nehWPkQKW9tml5sOhYUS1flg1LHM5pVgnOz6Nn5NipGxT5.
+language: en
+role: admin
+history:
+  - lineup/der-dose
+  - galerie
+  - lineup/sonic-the-machine
+  - lineup
+  - home
