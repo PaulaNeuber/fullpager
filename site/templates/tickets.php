4674c2c70fa7919d994f0d30968672b9286e9d02
Paula Neuber <p.neuber@zeppelin-university.net>
2016-04-20 17:17:41 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1d6e07ede2d83c0a31104d394232683773cf6d78, reversing
    changes made to 06c7ad67f42560b279be6597f159f61e848c8e8e.

--- site/templates/tickets.php
+++ site/templates/tickets.php
@@ -3,7 +3,6 @@
   <div class="title">
       <?php echo $page->headline()->kirbytext() ?>
     </div>
-<<<<<<< HEAD
     <div class="vvk">
       <table style="width:100%">
         <tr>
@@ -49,45 +48,5 @@
       </table>
     </div>
     <hr>
-=======
-    <div class="text">
-      <h1><?php echo $page->heading()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
-    <table style="width:100%">
-      <tr>
-        <td>  <img src="<?php echo url('assets/images/gessler.jpg') ?>" alt="<?php echo $site->title()->html() ?>" /> 
-        </td>
-        <td>  <img src="<?php echo url('assets/images/rathauscafe.jpg') ?>" alt="<?php echo $site->title()->html() ?>" /> 
-        </td>
-        <td>Zeppelin Universität
-        </td> 
-      </tr>
-      <tr>
-        <td>Buchhandlung Gessler 1862
-        </td>
-        <td>Café im Rathaus
-        </td>
-        <td>Zeppelin Universität
-        </td> 
-      </tr>
-      <tr>
-        <td>Friedrichstraße 53
-        </td>
-        <td>Adenauerplatz 1
-        </td>
-        <td>Fallenbrunnen 3
-        </td> 
-      </tr>
-      <tr>
-        <td>88045 Friedrichshafen
-        </td>
-        <td>88045 Friedrichshafen
-        </td>
-        <td>88045 Friedrichshafen
-        </td> 
-      </tr>
-    </table>
->>>>>>> origin/master
   </main>
 <?php snippet('footer') ?>
