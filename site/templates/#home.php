b26150d6e50074b4fbe2e6661ec1c183246b6f72
Paula Neuber <p.neuber@zeppelin-university.net>
2016-04-22 19:01:16 +0200

    Updated start page

---
+++ site/templates/#home.php
@@ -0,0 +1,24 @@
+<?php snippet('header') ?>
+
+  <main class="main" role="main">
+   
+    <div class="title">
+      <?php echo $page->title()->kirbytext() ?>
+    </div>
+   
+<table align=justify style="width:90%">
+  <tr>
+    <td align=left valign=top>
+    <div class="home">
+      <?php echo $page->text()->kirbytext() ?>
+    </div></td>
+    <td align=right valign=top><<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flangenachtfn%2F%3Ffref%3Dts&tabs&width=340&height=130&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="340" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></td>
+  </tr>
+</table>
+ <hr>
+  </main>
+
+
+
+
+<?php snippet('footer') ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -6,16 +6,11 @@
       <?php echo $page->title()->kirbytext() ?>
     </div>
    
-<table align=justify style="width:90%">
-  <tr>
-    <td align=left valign=top>
     <div class="home">
       <?php echo $page->text()->kirbytext() ?>
-    </div></td>
-    <td align=right valign=top><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flangenachtfn&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></td>
-  </tr>
-</table>
- <hr>
+
+    </div>
+ <hr id="start">
   </main>
 
 
