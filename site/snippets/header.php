0040ebd85d5c3195fb98269f6bdc4f958138b02b
Paula Neuber <p.neuber@zeppelin-university.net>
2016-04-23 13:52:09 +0200

    Bootstrap Implementation

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,16 +2,18 @@
 <html lang="en">
 <head>
   <link rel="shortcut icon" type="image/x-icon" href="http://lange-nacht-fn.de.w0146c92.kasserver.com/content/lndm/favicon.ico">
-  <!-- Latest compiled and minified CSS -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-
+  <title>
+    <?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>
+  </title>
+  <!-- Latest compiled and minified CSS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"> </script>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  
   <?php echo css('assets/css/main.css') ?>
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,400italic,700,600' rel='stylesheet' type='text/css'>
 </head>
