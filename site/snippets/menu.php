e3f39112344af3612925fe61e3cae47dda7909d1
Paula Neuber <p.neuber@zeppelin-university.net>
2016-04-26 10:35:55 +0200

    commit 13

---
+++ site/snippets/menu.php
@@ -0,0 +1,22 @@
+<nav class="navbar">
+  <ul class="menu cf">
+    <li>
+      <a <?php e($site->page('home')->isOpen(), ' class="active"') ?> href="<?php echo $site->url() ?>">Home</a>
+    </li>
+    <?php foreach($pages->visible() as $p): ?>
+      <li class="first">
+        <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+        <!--      <?php if($p->hasVisibleChildren()): ?>
+        <ul class="submenu">
+          <?php foreach($p->children()->visible() as $p): ?>
+          <li>
+            <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+          </li>
+          <?php endforeach ?>
+        </ul>
+        <?php endif ?> -->
+      </li>
+      <?php endforeach ?>
+  </ul>
+  </nav>
+
